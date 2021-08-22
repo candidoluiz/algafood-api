022cfa272f2f258054ecac5dc587161e03d98c0f
luiz <luiz.fernando360@gmail.com>
2021-08-22 15:24:57 -0300

    Aula 2.20

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -1,24 +1,22 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
-public class NotificadorEmail implements Notificador{
+public class NotificadorEmailMock implements Notificador{
 
-    public NotificadorEmail() {
-        System.out.println("Notificador email REAL");
+    public NotificadorEmailMock() {
+        System.out.println("Notificador email MOCK");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
 
-        System.out.printf("Notificando %s através do email %s: %s\n",
+        System.out.printf("MOCK: Notificação seria enviada para %s através do email %s: %s\n",
                 cliente.getNome(),cliente.getEmail(),mensagem);
     }
 
