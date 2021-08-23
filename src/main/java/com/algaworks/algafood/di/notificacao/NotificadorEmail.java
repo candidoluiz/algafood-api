473b320c002e15084c16f34a409fc7067c302f51
luiz <luiz.fernando360@gmail.com>
2021-08-23 19:55:56 -0300

    Fim do modulo 2

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
@@ -10,16 +11,14 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador{
-    @Value("${notificador.email.host-servidor}")
-    private String host;
 
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    private NotificadorProperties properties;
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        System.out.println("host: " + host);
-        System.out.println("porta: " + porta);
+        System.out.println("host: " + properties.getHostServidor());
+        System.out.println("porta: " + properties.getPortaServidor());
 
         System.out.printf("Notificando %s através do email %s: %s\n",
                 cliente.getNome(),cliente.getEmail(),mensagem);
