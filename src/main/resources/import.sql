e10a8d03c9e8f836d40b7c6dc1d6a5ab6e2c70b6
luiz <luiz.fernando360@gmail.com>
2021-08-25 20:22:47 -0300

    Mapeando relacionamento com @ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
-insert into restaurante (nome, taxa_frete) values ('Lagurmet',5.00)
-insert into restaurante (nome, taxa_frete) values ('Olivier',3.99)
+insert into cozinha (id,nome) values (1,'Tailandesa');
+insert into cozinha (id,nome) values (2,'Indiana');
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Lagurmet',5.00, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Olivier',3.99, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk',15.99, 2);
