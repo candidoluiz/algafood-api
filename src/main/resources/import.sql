d752fd826ab8d665e391271936b3b19b4194b792
luiz <luiz.fernando360@gmail.com>
2021-08-24 21:27:24 -0300

    Desafio: Lombok e repositório de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+insert into restaurante (nome, taxa_frete) values ('Lagurmet',5.00)
+insert into restaurante (nome, taxa_frete) values ('Olivier',3.99)
