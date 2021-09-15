ace4d9d078c536a408dbc6fa47227db6872177f0
luiz <luiz.fernando360@gmail.com>
2021-09-14 21:33:00 -0300

    Implementando uma consulta dinâmica com JPQL

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -67,7 +67,7 @@ public class TesteController {
     }
 
     @GetMapping("restaurantes/por-nome-e-frete")
-    public List<Restaurante> restaurantesPorNomeFrete(@RequestParam String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+    public List<Restaurante> restaurantesPorNomeFrete( String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
         return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
     }
 }
