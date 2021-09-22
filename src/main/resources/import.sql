6e4dbbc8ff6fee0b4c88cecdaf06b6bd99243490
luiz <luiz.fernando360@gmail.com>
2021-09-21 22:34:09 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,9 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
 
-insert into forma_pagamento(descricao) values ('A vista');
-insert into forma_pagamento(descricao) values ('Cartão');
+insert into forma_pagamento(descricao) values ('Cartão de crédito');
+insert into forma_pagamento(descricao) values ('Cartão de débito');
+insert into forma_pagamento(descricao) values ('Dinheiro');
 
 insert into permissao(nome, descricao) values ('Inserir','Permite inserir dados no banco de dados');
 
@@ -19,5 +20,7 @@ insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gurmet',10.
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery',0.00, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Olivier',3.99, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana',15.99, 2);
+
+insert into restaurante_forma_pagamento(restaurante_id, forma_pagamento_id) values (1,1), (1,2), (1,3), (2,3), (3,2), (3,3);
 
 
