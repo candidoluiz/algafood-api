14a9373091333d2162f2fbc3203e2b25dce6e23a
luiz <luiz.fernando360@gmail.com>
2021-10-25 19:10:46 -0300

    Desafio: implementando exception handler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
@@ -38,6 +39,16 @@ public class ApiExceptionHandler {
                 .dataHora(LocalDateTime.now())
                 .mensagem("O tipo de mídia não é aceito").build();
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(problema);
+
+    }
+
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?>  tratarEntidadeEmUsoException(EntidadeEmUsoException e){
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage()).build();
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(problema);
 
     }
