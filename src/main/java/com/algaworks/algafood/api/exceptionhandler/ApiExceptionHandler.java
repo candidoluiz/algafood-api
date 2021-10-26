630562048fc1c0b5ef484638769227f2edf01516
luiz <luiz.fernando360@gmail.com>
2021-10-26 20:35:28 -0300

    Habilitando erros na desserialização de propriedades inexistentes ou ignoradas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
