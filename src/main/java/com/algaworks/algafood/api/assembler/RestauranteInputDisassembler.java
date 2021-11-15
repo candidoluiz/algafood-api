9afc033ff61ea2d54f1a68833c65f0ab894bdf9a
luiz <luiz.fernando360@gmail.com>
2021-11-15 20:09:23 -0300

    Revisando e ajustando as mensagens de validação com o uso de DTOs

--- src/main/java/com/algaworks/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -21,7 +21,7 @@ public class RestauranteInputDisassembler {
         // Para evitar org.hibernate.HibernateException: identifier of an instance
         // of com.algaworks.algafood.domain.model.Cozinha was altered from 1 to 2
         restaurante.setCozinha(new Cozinha());
-        
+
         modelMapper.map(restauranteInput, restaurante);
     }
 }
