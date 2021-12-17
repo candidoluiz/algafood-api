1197fa7632b10f2eb55313b8147850054c337bb8
luiz <luiz.fernando360@gmail.com>
2021-12-17 13:41:14 -0300

    Refatorando o código de regras para transição de status de pedidos

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -74,5 +75,30 @@ public class Pedido {
                 .map(item -> item.getPrecoTotal())
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
         this.valorTotal = this.subtotal.add(this.taxaFrete);
+    }
+
+    public void confirmar(){
+        setStatus(StatusPedido.CONFIRMADO);
+        setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    public void entregar(){
+        setStatus(StatusPedido.ENTREGUE);
+        setDataEntrega(OffsetDateTime.now());
+    }
+
+    public void cancelar(){
+        setStatus(StatusPedido.CANCELADO);
+        setDataCancelamento(OffsetDateTime.now());
+    }
+
+    private void setStatus(StatusPedido novoStatus){
+        if (getStatus().naoPodeAlterarPara(novoStatus)){
+            throw new NegocioException(
+                    String.format("Status do pedido %d não pode alterado de %s para %s",
+                    getId(),getStatus().getDescricao(),novoStatus.getDescricao()));
+        }
+
+        this.status = novoStatus;
     }
 }
