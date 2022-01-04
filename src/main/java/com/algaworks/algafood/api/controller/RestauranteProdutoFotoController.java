c9fc2b29dd6dcf29bd336fe7ac3f12a68969ba93
luiz <luiz.fernando360@gmail.com>
2022-01-03 22:21:05 -0300

    Validando o tamanho máximo do arquivo

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -2,10 +2,12 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import javax.validation.Valid;
 import java.nio.file.Path;
 import java.util.UUID;
 
@@ -14,7 +16,7 @@ import java.util.UUID;
 public class RestauranteProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput){
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput){
 
         var nomeArquivo = UUID.randomUUID().toString()+"_"+fotoProdutoInput.getArquivo().getOriginalFilename();
         var arquivoFoto = Path.of("C:\\Users\\candi\\Desktop\\catalogo",nomeArquivo);
