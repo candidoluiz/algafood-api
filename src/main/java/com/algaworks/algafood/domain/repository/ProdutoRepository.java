3786816438f47eab70ac4cf85a20b7016fb89436
luiz <luiz.fernando360@gmail.com>
2022-01-06 20:37:26 -0300

    Excluindo e substituindo cadastro de foto de produto

--- src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.repository;
 
+import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,6 +21,9 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>, Produto
 
     @Query("from Produto p where p.ativo = true and p.restaurante = :restaurante")
     List<Produto> findAtivosByRestaurante(@Param("restaurante") Restaurante restaurante);
+
+    @Query("select f from FotoProduto f join f.produto p where p.restaurante.id = :restauranteId and f.produto.id = :produtoId")
+    Optional<FotoProduto> findFotoById(@Param("restauranteId")Long restauranteId, @Param("produtoId")Long produtoId);
 
 
 }
