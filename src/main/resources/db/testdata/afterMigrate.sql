0a687ad79efd92cf842df0851d6397e80da6484c
luiz <luiz.fernando360@gmail.com>
2022-01-17 21:10:31 -0300

    Melhorando o texto do e-mail com FTL (FreeMarker Template Language)

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -113,7 +113,7 @@ insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values 
 insert into pedido (codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
     status, data_criacao, subtotal, taxa_frete, valor_total)
-values ('2de56aec-efca-4d93-b75b-129642fb4794',1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
+values ('2de56aec-efca-4d93-b75b-129642fb4794',1, 6, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
 'CRIADO', timezone('utc', now()), 298.90, 10, 308.90);
 
 insert into item_pedido (pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
