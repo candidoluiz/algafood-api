bea6ea2a33cc5f6cfae576f286c43fb83a76b789
luiz <luiz.fernando360@gmail.com>
2022-08-21 22:03:20 -0300

    Desafio: refatorando serviços de usuários para usar BCrypt

--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -9,6 +9,7 @@ import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +26,9 @@ public class CadastroUsuarioService {
     @Autowired
     private CadastroGrupoService cadastroGrupoService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public Usuario salvar(Usuario usuario){
         // TODO Aula 12.11 uso do detach
@@ -36,16 +40,20 @@ public class CadastroUsuarioService {
             throw new NegocioException(
                     String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
         }
+        if (usuario.isNovo()){
+            usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        }
         return usuarioRepository.save(usuario);
     }
 
     @Transactional
     public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha){
         Usuario usuario = buscarOuFalhar(usuarioId);
-        if (usuario.senhaNaoCoincideCom(senhaAtual)){
+
+        if (!passwordEncoder.matches(senhaAtual, usuario.getSenha())){
             throw new NegocioException("Senha atual informada não coincide com a senha do usuário");
         }
-        usuario.setSenha(novaSenha);
+        usuario.setSenha(passwordEncoder.encode(novaSenha));
     }
     @Transactional
     public void excluir(Long usuarioId){
