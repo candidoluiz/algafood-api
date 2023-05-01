5b50cb14c0f86fc06923a8c600f8fd0153acb125
luiz <luiz.fernando360@gmail.com>
2023-04-30 22:29:13 -0300

    Implementando o endpoint do JSON Web Key Set (JWKS)

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/core/AutorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/core/AutorizationServerConfig.java
@@ -3,12 +3,9 @@ package com.algaworks.algafood.auth.core;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
